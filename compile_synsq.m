406d45db3e230ea2cd83414ed9a303b9f1afd3c8
S.Mostafa Mousavi <smousavi05@users.noreply.github.com>
2020-01-20 14:46:15 -0800

    Delete compile_synsq.m

--- compile_synsq.m
+++
@@ -1,9 +0,0 @@
-% Compile MEX files for Synchrosqueezing toolbox
-%
-%---------------------------------------------------------------------------------
-%    Synchrosqueezing Toolbox
-%    Authors: Eugene Brevdo (http://www.math.princeton.edu/~ebrevdo/)
-%---------------------------------------------------------------------------------
-mex curve_ext.c
-mex synsq_cwt_squeeze_mex.c
-mex diff_mex.c
